3eef61559ed41abd1dfabdc640fb9722dd5dfae5
Namory Traore <namorytraore1984@yahoo.fr>
2021-12-28 22:54:15 -0500

    1er commit

---
+++ src/main/java/com/next/technologies/ntaccountmanager/NtAccountManagerApplication.java
@@ -0,0 +1,13 @@
+package com.next.technologies.ntaccountmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NtAccountManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NtAccountManagerApplication.class, args);
+	}
+
+}
